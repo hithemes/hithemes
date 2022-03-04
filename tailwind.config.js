1c79c9405f160b7b2d437adef4a6f9ba76e1dcc2
Halit <halitguvenilir@gmail.com>
2022-03-04 18:39:13 +0300

    Prettier added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,23 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     fontWeight: {
-      "extra-light": 200,
+      'extra-light': 200,
       light: 300,
       normal: 400,
       medium: 500,
       semibold: 600,
       bold: 700,
-      "extra-bold": 800,
+      'extra-bold': 800
     },
     fontFamily: {
-      sans: ["Manrope", ...defaultTheme.fontFamily.sans],
-    },
+      sans: ['Manrope', ...defaultTheme.fontFamily.sans]
+    }
   },
-  plugins: [],
-};
+  plugins: [require('@tailwindcss/aspect-ratio')]
+}
