3bd9554f710a25c3af66e629a397c7106d217048
Halit <halitguvenilir@gmail.com>
2022-12-02 11:51:29 +0300

    Constats added

---
+++ constants/index.js
@@ -0,0 +1,47 @@
+export const TAB_MENU = [
+  {
+    id: 0,
+    name: 'My Library'
+  },
+  {
+    id: 1,
+    name: 'Billing'
+  },
+  {
+    id: 2,
+    name: 'Settings'
+  },
+  {
+    id: 3,
+    name: 'Review'
+  },
+  {
+    id: 4,
+    name: 'Support'
+  }
+]
+
+export const PRODUCT_LIST = [
+  {
+    id: 0,
+    name: 'Capsule',
+    website: 'getcapsule.com',
+    title: 'Design software',
+    miniTitle: 'Super lightweight design app',
+    version: '1.0.0',
+    licence: 'Licence  Key Documentation.pdf',
+    image:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+  },
+  {
+    id: 1,
+    name: 'Command+R',
+    website: 'cmdr.ai',
+    title: 'Data prediction',
+    miniTitle: 'AI and machine learning data',
+    version: '3.55.3',
+    licence: 'Licence  Key Documentation.pdf',
+    image:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+  }
+]
