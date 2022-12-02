82e344366dbd7908f01f0283487e17077ad57bd6
Halit <halitguvenilir@gmail.com>
2022-12-02 11:51:19 +0300

    SVGR package and icons added

---
+++ components/icons/Download.js
@@ -0,0 +1,20 @@
+import * as React from 'react'
+const SvgDownload = (props) => (
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 21 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M7.167 14.167 10.5 17.5m0 0 3.334-3.333M10.5 17.5V10m6.667 3.952a4.583 4.583 0 0 0-2.917-8.12.516.516 0 0 1-.445-.25 6.25 6.25 0 1 0-9.816 7.58"
+      stroke="currentColor"
+      strokeWidth={1.667}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)
+export default SvgDownload
