5af7da19d2c67a48010bf696b04687fb56990f57
Halit Güvenilir <halitguvenilir@gmail.com>
2022-12-16 13:18:07 +0300

    New product list added

--- constants/index.js
+++ constants/index.js
@@ -43,6 +43,61 @@ export const PRODUCT_LIST = [
     licence: 'Licence  Key Documentation.pdf',
     image:
       'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+  },
+  {
+    id: 2,
+    name: 'Command+R',
+    website: 'cmdr.ai',
+    title: 'Data prediction',
+    miniTitle: 'AI and machine learning data',
+    version: '3.55.3',
+    licence: 'Licence  Key Documentation.pdf',
+    image:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+  },
+  {
+    id: 3,
+    name: 'Command+R',
+    website: 'cmdr.ai',
+    title: 'Data prediction',
+    miniTitle: 'AI and machine learning data',
+    version: '3.55.3',
+    licence: 'Licence  Key Documentation.pdf',
+    image:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+  },
+  {
+    id: 4,
+    name: 'Command+R',
+    website: 'cmdr.ai',
+    title: 'Data prediction',
+    miniTitle: 'AI and machine learning data',
+    version: '3.55.3',
+    licence: 'Licence  Key Documentation.pdf',
+    image:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+  },
+  {
+    id: 5,
+    name: 'Command+R',
+    website: 'cmdr.ai',
+    title: 'Data prediction',
+    miniTitle: 'AI and machine learning data',
+    version: '3.55.3',
+    licence: 'Licence  Key Documentation.pdf',
+    image:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+  },
+  {
+    id: 6,
+    name: 'Command+R',
+    website: 'cmdr.ai',
+    title: 'Data prediction',
+    miniTitle: 'AI and machine learning data',
+    version: '3.55.3',
+    licence: 'Licence  Key Documentation.pdf',
+    image:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
   }
 ]
 
